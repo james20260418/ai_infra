e9079709fd871873872a018d3b1c445f9d69a702
james20260418 <thy_james_20260418@163.com>
2026-04-24 13:31:21 +0800

    test: add unit tests for segment_2, polyline_2d, aabox2
    - Extract AABox2 from geometry_legacy to geom/2d/aabox2.h (namespace geom)
    - geometry_legacy.h now provides backward-compatible aliases
    - 15 Segment2 tests: construction, projection, point-on, distance, shift
    - 12 Polyline2d tests: construction, vertex access, s-param, interpolation, clamping
    - 9 AABox2 tests: construction, spans, edge cases, float type support
    - All 40 tests pass

--- geom/2d/geometry_legacy.h
+++ geom/2d/geometry_legacy.h
@@ -1,50 +1,19 @@
 #pragma once
 
+// AABox2 has been moved to geom/2d/aabox2.h under namespace geom.
+// This legacy header remains for backward compatibility.
+// New code should include "geom/2d/aabox2.h" and use geom::AABox2 instead.
+#include "geom/2d/aabox2.h"
+
 #include <optional> 
 
 #include "geom/common/check.h"
 #include "geom/common/vec.h"
 
-namespace geom_legacy { 
-	
+namespace geom_legacy {
 
-	template<typename T>
-	struct AABox2 {
-	public:
-		AABox2(T x_min, T x_max, T y_min, T y_max) :
-			x_min_(x_min),
-			x_max_(x_max),
-			y_min_(y_min),
-			y_max_(y_max) {
-			DCHECK_GE(x_max_, x_min_);
-			DCHECK_GE(y_max_, y_min_);
-		}
-
-		inline double x_min() const {
-			return x_min_;
-		}
-		inline double x_max() const {
-			return x_max_;
-		}
-		inline double XSpan() const {
-			return x_max_ - x_min_;
-		}
-
-		inline double y_min() const {
-			return y_min_;
-		}
-		inline double y_max() const {
-			return y_max_;
-		}
-		inline double YSpan() const {
-			return y_max_ - y_min_;
-		}
-
-		T x_min_ = 0.0;
-		T x_max_ = 0.0;
-		T y_min_ = 0.0;
-		T y_max_ = 0.0;
-	};
-	using AABox2d = AABox2<double>;
+// Backward-compatible alias to the new location.
+using geom::AABox2;
+using geom::AABox2d;
 
 }  // namespace geom_legacy
